73f5d67415cc73c4e016983470aaefee9e2e1ef2
Ethan Barrett <hello@buildingsareheavy.com>
2018-11-25 22:16:12 -0700

    styled, full copy and navigation is working via js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
+//
+// Start Navigation
+//
+
+const navArray = ["index", "about", "submit"];
+
+function addNav(linkText, linkHref) {
+  const navContainer = document.getElementById("mainNav");
+  const navLink = document.createElement("a");
+  const navItem = document.createElement("li");
+  navLink.text = linkText;
+  navLink.href = linkHref;
+
+  navContainer.appendChild(navItem).appendChild(navLink);
+}
+
+navArray.forEach(function(navItem) {
+  text = navItem;
+  href = "./" + navItem + ".html";
+
+  addNav(text, href);
+});
+
+console.log(navArray);
+
+//
+// Start Google Sheets API
+// Only needed on index.html, no need to load on other pages
+//
+
 fetch(
   "https://sheets.googleapis.com/v4/spreadsheets/1rmcpC1s-oKdzlh3CEgDTrmTYv2Xlk1S9XbRGWZuEB3M/values/Sheet1?valueRenderOption=FORMATTED_VALUE&key=AIzaSyA0OjajGZQT0ybehn5xbSfwalZnxVT4bjQ"
 )
@@ -5,26 +35,36 @@ fetch(
     return response.json();
   })
   .then(data => {
-    var postArray = data.values;
+    const postArray = data.values;
 
     function addPost(addName, addText) {
-      var mainContainer = document.getElementById("posts");
-      var postContainer = document.createElement("div");
-      var postName = document.createElement("p");
-      var postText = document.createElement("p");
+      const mainContainer = document.getElementById("posts");
+      const postContainer = document.createElement("div");
+      const postName = document.createElement("p");
+      const postText = document.createElement("p");
+
+      // Add class to each p tag
+      postName.classList.add("post-name");
+      postText.classList.add("post-text");
 
       postName.innerText = addName;
       postText.innerText = addText;
-      mainContainer
-        .appendChild(postContainer)
-        .appendChild(postName)
-        .appendChild(postText);
+      mainContainer.appendChild(postContainer).appendChild(postText);
+      mainContainer.appendChild(postContainer).appendChild(postName);
     }
 
     // forEach for each column
     postArray.forEach(function(column) {
       name = column[0];
       text = column[1];
+
+      if (text == undefined) {
+        text = "Intentionally left blank.";
+      }
+
+      if (name == "") {
+        name = "Anonymous";
+      }
 
       // Display name and text
       addPost(name, text);
