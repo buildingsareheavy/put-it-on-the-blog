61efaf0b0fd6a43caccc91743e4e1c1080e01f21
Ethan Barrett <hello@buildingsareheavy.com>
2018-11-28 16:23:08 -0700

    refactored js and scss to be separate files, and added open graph meta tags

--- js/blog.js
+++ js/blog.js
@@ -1,27 +1,4 @@
 //
-// Start Navigation
-//
-
-const navArray = ["index", "about", "submit"];
-
-function addNav(linkText, linkHref) {
-  const navContainer = document.getElementById("mainNav");
-  const navLink = document.createElement("a");
-  const navItem = document.createElement("li");
-  navLink.text = linkText;
-  navLink.href = linkHref;
-
-  navContainer.appendChild(navItem).appendChild(navLink);
-}
-
-navArray.forEach(function(navItem) {
-  text = navItem;
-  href = "./" + navItem + ".html";
-
-  addNav(text, href);
-});
-
-//
 // Start Google Sheets API
 // Only needed on index.html, no need to load on other pages
 //
