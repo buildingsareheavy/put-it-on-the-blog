427c5c18f5c774e65e72a549c0266f544551d3ff
Ethan Barrett <hello@buildingsareheavy.com>
2019-05-12 22:05:52 -0600

    added post capibilities

---
+++ src/views/Post.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div>
+      <button @click="postQuote">Post new entry</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "post",
+  methods: {
+    postQuote() {
+      return axios({
+        method: "post",
+        url: "https://api.airtable.com/v0/apprxCAgCvkroIzyQ/Table%201",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + process.env.VUE_APP_API_KEY
+        },
+        data: {
+          fields: {
+            Name: "Ethan",
+            Quote: "Hey! I can post!"
+          }
+        }
+      })
+        .then(function(response) {
+          console.log("yay");
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log("nope");
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+button {
+  text-transform: uppercase;
+  font-size: 2rem;
+  margin: 1rem;
+}
+</style>
